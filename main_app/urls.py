f7de8d4ee60433502122ed79b7a80cc2619d5aa6
ssemlitz <sdsemlitz@yahoo.com>
2022-08-08 10:17:50 -0400

    fish index template

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,9 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('about/', views.about, name='about')
+  path('about/', views.about, name='about'),
+  path('fishies/', views.fishies_index, name='fishies_index'),
 ]
