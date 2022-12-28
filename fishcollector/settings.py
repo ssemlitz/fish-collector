4386bef9e86472aa6338b010fb2d22eb5a141c69
ssemlitz <sdsemlitz@yahoo.com>
2022-12-28 13:10:35 -0500

    bitio setup

--- fishcollector/settings.py
+++ fishcollector/settings.py
@@ -83,9 +83,12 @@ WSGI_APPLICATION = 'fishcollector.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        # 'NAME': os.environ.get('POSTGRES_NAME'),
+        # 'USER': os.environ.get('POSTGRES_USER'),
+        # 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'NAME': 'fishcollector',
+        'USER': 'ssemlitz',
+        'PASSWORD': os.environ.get('POSTGRES_BITIO_PASSWORD'),
         'HOST': 'db.bit.io',
         'PORT': 5432,
     }
